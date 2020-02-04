a7f2ecfabeaac317841bd573720e1558ea6f9c1e
Satyam Soni <49560@hexaware.com>
2020-02-04 17:06:42 +0530

    Initial commit

---
+++ webapi1/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using webapi1.Models;
+namespace webapi1.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> values { get; set; }
+
+
+    }
+}
---
+++ webapi1/Models/Value.cs
@@ -0,0 +1,9 @@
+namespace webapi1.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
